afabbf6dc002cb30144f74e0ab23f0b2405f8c2d
LenaFedchenko <lenafedcenko98@gmail.com>
2026-02-12 21:16:45 +0200

    added class for card

---
+++ modules/card.py
@@ -0,0 +1,55 @@
+from PyQt6 import QtWidgets as widgets
+import PyQt6.QtCore as core
+from .frame import Frame_create
+
+class Card(widgets.QFrame):
+    def __init__(self):
+        super().__init__()
+        self.setFixedSize(core.QSize(330, 98))
+        self.setStyleSheet("""
+                            background-color: rgba(0, 0, 0, 51); 
+                            border-radius: 8px;
+                            """)
+        self.LAYOUT = widgets.QVBoxLayout()
+        self.LAYOUT.setContentsMargins(8, 8, 8, 8)
+        self.setLayout(self.LAYOUT)
+
+        self.FRAME_LAYOUT = widgets.QHBoxLayout()
+        self.FRAME_LAYOUT.setContentsMargins(0, 0, 0, 8)
+        self.FRAME2_LAYOUT = widgets.QHBoxLayout()
+        frame = Frame_create(layout=self.FRAME_LAYOUT, width=314, height=52)
+        frame.setStyleSheet("background-color: transparent")
+        frame2 = Frame_create(layout=self.FRAME2_LAYOUT, width=314)
+        self.FRAME2_LAYOUT.setContentsMargins(0, 0, 0, 0)
+        frame2.setStyleSheet("background-color: transparent")
+        self.LAYOUT.addWidget(frame)
+        self.LAYOUT.addWidget(frame2)
+        
+
+        self.LAYOUT_FRAME3 = widgets.QVBoxLayout()
+        frame3 = Frame_create(layout=self.LAYOUT_FRAME3, width=247, height=58)
+        frame3.setStyleSheet("background-color: transparent;")
+        self.LAYOUT_FRAME3.setContentsMargins(2, 0, 0, 7)
+        self.LAYOUT_DEGREE = widgets.QHBoxLayout()
+        frame_degree = Frame_create(layout=self.LAYOUT_DEGREE, width=67, height=52)
+        self.DEGREE = widgets.QLabel(parent= frame_degree, text= "11°") 
+        self.DEGREE.setStyleSheet("font-size: 44px; background-color: transparent") 
+
+        self.CITY_NAME = widgets.QLabel(parent= frame3, text="Рим")
+        self.TIME = widgets.QLabel(parent= frame3, text="20:15")
+        self.CITY_NAME.setStyleSheet("font-size: 24px; background-color: transparent")
+        self.TIME.setStyleSheet("font-size: 12px; background-color: transparent")
+
+        self.WEATER_FEELING = widgets.QLabel(parent=frame2, text= "Переважно хмарно")
+        self.TEMP = widgets.QLabel(parent=frame2, text="Макс.:11°, мін.:0°s")
+        self.WEATER_FEELING.setStyleSheet("font-size: 12px; background-color: transparent")
+        self.TEMP.setStyleSheet("font-size: 12px; background-color: transparent")
+        self.TEMP.setAlignment(core.Qt.AlignmentFlag.AlignRight)
+        self.WEATER_FEELING.setAlignment(core.Qt.AlignmentFlag.AlignLeft)
+
+        self.FRAME2_LAYOUT.addWidget(self.WEATER_FEELING)
+        self.FRAME2_LAYOUT.addWidget(self.TEMP)
+        self.LAYOUT_FRAME3.addWidget(self.CITY_NAME)
+        self.LAYOUT_FRAME3.addWidget(self.TIME)
+        self.FRAME_LAYOUT.addWidget(frame3)
+        self.FRAME_LAYOUT.addWidget(frame_degree)
--- modules/scroll_frame.py
+++ modules/scroll_frame.py
@@ -1,25 +1,21 @@
 import PyQt6.QtWidgets as widgets
+from .card import Card
 import PyQt6.QtCore as core
-
-from .load_img import ImageLoad
 
 class Scroll_frame(widgets.QScrollArea):
     def __init__(self, parent):
         widgets.QScrollArea.__init__(self, parent = parent)
+        self.setVerticalScrollBarPolicy(core.Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         self.setFixedSize(370, 707)
-        self.setStyleSheet("border: none;")
+        self.setStyleSheet("border: none; background-color: transparent")
         self.setWidgetResizable(True)
-        self.setStyleSheet("background-color: transparent")
         self.SCROLL_FRAME = widgets.QFrame(parent = self)
         self.SCROLL_FRAME_LAYOUT = widgets.QVBoxLayout()
         self.SCROLL_FRAME.setLayout(self.SCROLL_FRAME_LAYOUT)
         self.setWidget(self.SCROLL_FRAME)
         
         for i in range(10):
-            card = widgets.QFrame(parent = self.SCROLL_FRAME)
-            card.setFixedSize(core.QSize(330, 90))
-            card.setStyleSheet("background-color: red; ")
-            
+            card = Card()
             self.SCROLL_FRAME_LAYOUT.addWidget(card)
 
-        
+
