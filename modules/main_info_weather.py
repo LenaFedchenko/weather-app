fffd89e1ec80478adff4a7d8f3d2d6e3d4b19ac4
LenaFedchenko <lenafedcenko98@gmail.com>
2026-02-26 18:18:18 +0200

    added design

--- modules/main_info_weather.py
+++ modules/main_info_weather.py
@@ -8,71 +8,117 @@ from .load_img import ImageLoad
 class MainInfoWeather():
     def __init__(self, content_frame):
         self.FRAME_LAYOUT = widgets.QHBoxLayout()
-        frame_all = Frame_create(layout= self.FRAME_LAYOUT, width=788, height=303)
         self.FRAME_LAYOUT.setContentsMargins(0, 0, 0, 0)
+
+        frame_all = Frame_create(layout=self.FRAME_LAYOUT, width=788, height=303)
         content_frame.addWidget(frame_all)
 
         WEATHER_LAYOUT = widgets.QVBoxLayout()
-        # WEATHER_LAYOUT.setAlignment(core.Qt.AlignmentFlag.AlignCenter)
-        POSITION_LAYOUT = widgets.QHBoxLayout()
-        POSITION_LAYOUT.setAlignment(core.Qt.AlignmentFlag.AlignLeft)
-        # POSITION_LAYOUT.setContentsMargins(0, 0, 0, 0)
+        WEATHER_LAYOUT.setAlignment(core.Qt.AlignmentFlag.AlignTop)
+
         WEATHER = Frame_create(WEATHER_LAYOUT, 390, 303)
-        WEATHER.setStyleSheet('background-color: rgba(0, 0, 0, 51); border-radius: 20px; font-weight: 500')
+        WEATHER.setStyleSheet("""
+            background-color: rgba(0, 0, 0, 51);
+            border-radius: 20px;
+        """)
+
         self.FRAME_LAYOUT.addWidget(WEATHER)
-        POSITION = Frame_create(POSITION_LAYOUT, 358, 37)
-        POSITION.setStyleSheet('background-color: transparent')
-        self.LABEL = widgets.QLabel(POSITION, text = 'Поточна позиція')
-        self.LABEL.setStyleSheet('font-size: 16px; background-color: transparent')
-        self.LABEL_ICON = ImageLoad(16, 16, POSITION, 'location.png')
-        POSITION_LAYOUT.addWidget(self.LABEL_ICON)
-        # self.LABEL_ICON.setStyleSheet('background-color: transparent')
-        degree_img_layout = widgets.QHBoxLayout()
-        degree_img = Frame_create(layout=degree_img_layout, width=210, height= 87, color="transparent")
-        self.LABEL_1 = widgets.QLabel(WEATHER, text = 'Дніпро')
-        self.LABEL_1.setStyleSheet('font-size: 44px; background-color: transparent')
-        img_weather = ImageLoad(96, 96, degree_img, 'example.png')
-        degree_img_layout.addWidget(img_weather)
-        aligment_frame_layout = widgets.QVBoxLayout()
-        aligment_frame_layout.setAlignment(core.Qt.AlignmentFlag.AlignCenter)
-        aligment_frame = Frame_create(layout= aligment_frame_layout, width=363, height=245)
-        aligment_frame.setStyleSheet("background-color: transparent")
-        aligment_frame_layout.addWidget(degree_img)
-        self.LABEL_2 = widgets.QLabel(WEATHER, text = '11°')
-        self.LABEL_2.setStyleSheet('font-size: 74px; background-color: transparent')
-        self.LABEL_3 = widgets.QLabel(WEATHER, text = 'Хмарно')
-        self.LABEL_3.setStyleSheet('font-size: 24px; background-color: transparent')
-        self.LABEL_4 = widgets.QLabel(WEATHER, text = 'Макс.:11°, мін.:0°')
-        self.LABEL_4.setStyleSheet('font-size: 16px; background-color: transparent')
+
+        POSITION_LAYOUT = widgets.QVBoxLayout()
+        POSITION_LAYOUT.setContentsMargins(0, 0, 0, 0)
+        POSITION_LAYOUT.setSpacing(5)
+
+        POSITION = Frame_create(POSITION_LAYOUT, 358, 50, "transparent")
+
+        group_layout = widgets.QHBoxLayout()
+        group_layout.setAlignment(core.Qt.AlignmentFlag.AlignLeft)
+        group_layout.setSpacing(5)
+
+        group = Frame_create(group_layout, 358, 40, "transparent")
+
+        self.LABEL_ICON = ImageLoad(16, 16, group, 'location.png')
+
+        self.LABEL = widgets.QLabel("Поточна позиція")
+        self.LABEL.setStyleSheet("font-size: 16px; background-color: transparent")
+
+        group_layout.addWidget(self.LABEL_ICON)
+        group_layout.addWidget(self.LABEL)
+
+        line = widgets.QFrame()
+        line.setFixedHeight(1)
+        line.setStyleSheet("background-color: white; border-radius: 1px;")
+
+        POSITION_LAYOUT.addWidget(group)
+        POSITION_LAYOUT.addWidget(line)
+        self.LABEL_1 = widgets.QLabel("Дніпро")
+        self.LABEL_1.setStyleSheet("font-size: 44px; background-color: transparent")
         self.LABEL_1.setAlignment(core.Qt.AlignmentFlag.AlignCenter)
+
+        degree_layout = widgets.QHBoxLayout()
+        degree_layout.setAlignment(core.Qt.AlignmentFlag.AlignCenter)
+
+        degree_frame = Frame_create(degree_layout, 300, 100, color="transparent")
+
+        img_weather = ImageLoad(96, 96, degree_frame, 'example.png')
+        self.LABEL_2 = widgets.QLabel("11°")
+        self.LABEL_2.setStyleSheet("font-size: 74px; background-color: transparent")
         self.LABEL_2.setAlignment(core.Qt.AlignmentFlag.AlignCenter)
+
+        degree_layout.addWidget(img_weather)
+        degree_layout.addWidget(self.LABEL_2)
+
+        self.LABEL_3 = widgets.QLabel("Хмарно")
+        self.LABEL_3.setStyleSheet("font-size: 24px; background-color: transparent")
         self.LABEL_3.setAlignment(core.Qt.AlignmentFlag.AlignCenter)
+
+        self.LABEL_4 = widgets.QLabel("Макс.:11°, мін.:0°")
+        self.LABEL_4.setStyleSheet("font-size: 16px; background-color: transparent")
         self.LABEL_4.setAlignment(core.Qt.AlignmentFlag.AlignCenter)
-        POSITION_LAYOUT.addWidget(self.LABEL)
+
         WEATHER_LAYOUT.addWidget(POSITION)
-        WEATHER_LAYOUT.addWidget(aligment_frame)
-        degree_img_layout.addWidget(self.LABEL_2)
-        aligment_frame_layout.addWidget(self.LABEL_1) 
-        aligment_frame_layout.addWidget(degree_img)  
-        aligment_frame_layout.addWidget(self.LABEL_3)      
-        aligment_frame_layout.addWidget(self.LABEL_4)
+        WEATHER_LAYOUT.addWidget(self.LABEL_1)
+        WEATHER_LAYOUT.addWidget(degree_frame)
+        WEATHER_LAYOUT.addWidget(self.LABEL_3)
+        WEATHER_LAYOUT.addWidget(self.LABEL_4)
         
 
         FRAME_LAYUOT1 = widgets.QVBoxLayout()
         frame_date = Frame_create(layout=FRAME_LAYUOT1, width=390, height=303) 
+        
         frame_date.setStyleSheet('background-color: rgba(0, 0, 0, 56); border-radius: 20px')
         self.FRAME_LAYOUT.addWidget(frame_date)
-        lay_line =widgets.QVBoxLayout()
-        self.LABEL2 = widgets.QLabel(parent = frame_date, text = "Сегодня")
-        frame_line = Frame_create(layout=lay_line, width=358, height=2, color="white")
-        lay_time_date = widgets.QHBoxLayout()
-        frame_date_time = Frame_create(layout= lay_time_date, width=358, height=44, color="green")
-        self.LABEL3 = widgets.QLabel(parent = frame_date, text= "Понедилок")
-        self.LABEL4 = widgets.QLabel(parent = frame_date, text= "24.02.26")
-        lay_time_date.addWidget(self.LABEL3)
-        lay_time_date.addWidget(self.LABEL4)
-        FRAME_LAYUOT1.addWidget(self.LABEL2)
-        FRAME_LAYUOT1.addWidget(frame_line)
-        FRAME_LAYUOT1.addWidget(frame_date_time)
-        FRAME_LAYUOT1.setAlignment(core.Qt.AlignmentFlag.AlignLeft)
-        
+
+        group2_layout = widgets.QVBoxLayout()
+        group2 = Frame_create(group2_layout, 358, 45, color= "transparent")
+
+        self.TODAY = widgets.QLabel("Сьогодні")
+        self.TODAY.setStyleSheet("font-size: 16px")
+        line2 = widgets.QFrame()
+        line2.setFixedHeight(1)
+        line2.setStyleSheet("background-color: white;")
+
+        group2_layout.addWidget(self.TODAY)
+        group2_layout.addWidget(line2)
+
+        group3_layout = widgets.QHBoxLayout()
+        group3 = Frame_create(group3_layout, 358, 44, color= "transparent")
+
+        day = widgets.QLabel("Понеділок")
+        date = widgets.QLabel("24.03.2025")
+        day.setStyleSheet("font-size: 24px")
+        date.setStyleSheet("font-size: 24px")
+        group3_layout.addWidget(day)
+        group3_layout.addStretch()
+        group3_layout.addWidget(date)
+
+        clock_layout = widgets.QVBoxLayout()
+        clock = Frame_create(clock_layout, width=168, height= 168)
+        clock_layout.setAlignment(core.Qt.AlignmentFlag.AlignCenter)
+        clock.setStyleSheet(f"""
+                            border-radius: 80px; 
+                            background-image: url({os.path.abspath(os.path.join(__file__, "..", "..", "media", "Ticks.png"))});
+                            """)
+
+        FRAME_LAYUOT1.addWidget(group2)
+        FRAME_LAYUOT1.addWidget(group3)
+        FRAME_LAYUOT1.addWidget(clock, alignment=core.Qt.AlignmentFlag.AlignCenter)
