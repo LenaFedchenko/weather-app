ae5356003da795a476de8266fac386e87970aebe
LenaFedchenko <lenafedcenko98@gmail.com>
2026-04-01 23:23:37 +0300

    added switchin to cards modal

--- modules/settings_main_part.py
+++ modules/settings_main_part.py
@@ -40,130 +40,121 @@ class Main_part_settings(widgets.QFrame):
         self.BUTTONS_FRAME_LAYOUT.addWidget(self.size_app)
         self.BUTTONS_FRAME_LAYOUT.addWidget(self.APP_LANGUAGE)
         self.BUTTONS_FRAME_LAYOUT.addWidget(self.IMAGE_LIST)
-        
+        self.SEARCH_CITY = ""
+        self.LANGUAGE= ""
+        self.FRAME_IMAGE= ""
+        self.SIZE_APP = ""
         
 
     def search_city_pressed(self):
-        # try:
-            # self.SIZE_APP.setParent(None)
-            # self.LANGUAGE.setParent(None)
-            # self.FRAME_IMAGE.setParent(None)
-        # except:
-            self.search_layout = widgets.QVBoxLayout()
-            self.SEARCH_CITY = Frame_create(layout = self.search_layout, width = 554, height = 588, color = 'transparent')
-            self.FRAME_MAIN_LAUYT.addWidget(self.SEARCH_CITY)
-            
-            self.search_city_layout = widgets.QHBoxLayout()
-            self.search_city = Frame_create(self.search_city_layout, 544, 331, "transparent")
-            
-            self.added_cities_layout = widgets.QVBoxLayout()
-            self.added_cities = Frame_create(self.added_cities_layout, 524, 197, "pink")
+        self.del_prev_card(self.SEARCH_CITY)
+        # self.search_city.setStyleSheet("background-color: rgba(0, 0, 0, 100); border-radius: 5px")
+        self.search_layout = widgets.QVBoxLayout()
+        self.SEARCH_CITY = Frame_create(layout = self.search_layout, width = 554, height = 588, color = 'transparent')
+        self.FRAME_MAIN_LAUYT.addWidget(self.SEARCH_CITY)
+        
+        self.search_city_layout = widgets.QHBoxLayout()
+        self.search_city = Frame_create(self.search_city_layout, 544, 331, "transparent")
+        
+        self.added_cities_layout = widgets.QVBoxLayout()
+        self.added_cities = Frame_create(self.added_cities_layout, 524, 197, "pink")
 
-            
-            self.data_city_layout = widgets.QVBoxLayout()
-            self.data_city_layout.setAlignment(core.Qt.AlignmentFlag.AlignLeft)
-            self.data_city = Frame_create(self.data_city_layout, 239, 301, "transparent")
+        
+        self.data_city_layout = widgets.QVBoxLayout()
+        self.data_city_layout.setAlignment(core.Qt.AlignmentFlag.AlignLeft)
+        self.data_city = Frame_create(self.data_city_layout, 239, 301, "transparent")
 
-            # Пошук міста
-            self.LABEL = widgets.QLabel("Пошук міста")
+        # Пошук міста
+        self.LABEL = widgets.QLabel("Пошук міста")
 
-            self.map_layout = widgets.QVBoxLayout()
-            self.map = Frame_create(self.map_layout, 290, 301, "yellow")
+        self.map_layout = widgets.QVBoxLayout()
+        self.map = Frame_create(self.map_layout, 290, 301, "yellow")
 
-            self.block_inputs_layout = widgets.QVBoxLayout()
-            self.block_inputs = Frame_create(self.block_inputs_layout, 239, 194, "transparent")
+        self.block_inputs_layout = widgets.QVBoxLayout()
+        self.block_inputs = Frame_create(self.block_inputs_layout, 239, 194, "transparent")
 
-            self.LABEL_COUNTRY = widgets.QLabel("Країна")
-            self.box_countre = widgets.QComboBox(parent = self.data_city)
-            self.box_countre.setStyleSheet("background-color: white; color: black; border-radius: 4px")
-            self.box_countre.setFixedSize(209, 32)
-            self.box_countre.addItem("Італія")
-            self.box_countre.addItem("Пункт2")
-            self.box_countre.addItem("Пункт3")
+        self.LABEL_COUNTRY = widgets.QLabel("Країна")
+        self.box_countre = widgets.QComboBox(parent = self.data_city)
+        self.box_countre.setStyleSheet("background-color: white; color: black; border-radius: 4px")
+        self.box_countre.setFixedSize(209, 32)
+        self.box_countre.addItem("Італія")
+        self.box_countre.addItem("Пункт2")
+        self.box_countre.addItem("Пункт3")
 
-            self.LABEL_CITY = widgets.QLabel("Місто")
-            self.box_city = widgets.QComboBox(parent = self.data_city)
-            self.box_city.setStyleSheet("background-color: white; color: black; border-radius: 4px")
-            self.box_city.setFixedSize(209, 32)
-            self.box_city.addItem("Виберіть місто")
-            self.box_city.addItem("Пункт2")
-            self.box_city.addItem("Пункт3")
-            self.box_city.setFixedSize(229, 32)
-            
-            self.LABEL_COORDINATE = widgets.QLabel("Кординати")
-            self.box_coordinate = widgets.QComboBox(parent = self.data_city)
-            self.box_coordinate.setStyleSheet("background-color: white; color: black; border-radius: 4px")
-            self.box_coordinate.setFixedSize(209, 32)
-            self.box_coordinate.addItem("(WGS 84,UTM,MGRS)")
-            self.box_coordinate.addItem("Пункт2")
-            self.box_coordinate.addItem("Пункт3")
-            self.box_coordinate.setFixedSize(229, 32)
+        self.LABEL_CITY = widgets.QLabel("Місто")
+        self.box_city = widgets.QComboBox(parent = self.data_city)
+        self.box_city.setStyleSheet("background-color: white; color: black; border-radius: 4px")
+        self.box_city.setFixedSize(209, 32)
+        self.box_city.addItem("Виберіть місто")
+        self.box_city.addItem("Пункт2")
+        self.box_city.addItem("Пункт3")
+        self.box_city.setFixedSize(229, 32)
+        
+        self.LABEL_COORDINATE = widgets.QLabel("Кординати")
+        self.box_coordinate = widgets.QComboBox(parent = self.data_city)
+        self.box_coordinate.setStyleSheet("background-color: white; color: black; border-radius: 4px")
+        self.box_coordinate.setFixedSize(209, 32)
+        self.box_coordinate.addItem("(WGS 84,UTM,MGRS)")
+        self.box_coordinate.addItem("Пункт2")
+        self.box_coordinate.addItem("Пункт3")
+        self.box_coordinate.setFixedSize(229, 32)
 
-            self.SAVE_BUTTON = widgets.QPushButton("Зберегти")
-            self.SAVE_BUTTON.setFixedSize(105, 38)
-            self.SAVE_BUTTON.setStyleSheet("background-color: rgb(0, 0, 51); color: white; border-radius: 4px")
+        self.SAVE_BUTTON = widgets.QPushButton("Зберегти")
+        self.SAVE_BUTTON.setFixedSize(105, 38)
+        self.SAVE_BUTTON.setStyleSheet("background-color: rgb(0, 0, 51); color: white; border-radius: 4px")
 
 
-            self.data_city_layout.setAlignment(core.Qt.AlignmentFlag.AlignLeft)
-            self.block_inputs_layout.setAlignment(core.Qt.AlignmentFlag.AlignLeft)
-            self.block_inputs_layout.addWidget(self.LABEL_COUNTRY, alignment=core.Qt.AlignmentFlag.AlignLeft)
-            self.block_inputs_layout.addWidget(self.box_countre, alignment=core.Qt.AlignmentFlag.AlignLeft)
+        self.data_city_layout.setAlignment(core.Qt.AlignmentFlag.AlignLeft)
+        self.block_inputs_layout.setAlignment(core.Qt.AlignmentFlag.AlignLeft)
+        self.block_inputs_layout.addWidget(self.LABEL_COUNTRY, alignment=core.Qt.AlignmentFlag.AlignLeft)
+        self.block_inputs_layout.addWidget(self.box_countre, alignment=core.Qt.AlignmentFlag.AlignLeft)
 
-            self.block_inputs_layout.addWidget(self.LABEL_CITY, alignment=core.Qt.AlignmentFlag.AlignLeft)
-            self.block_inputs_layout.addWidget(self.box_city, alignment=core.Qt.AlignmentFlag.AlignLeft)
-            self.data_city_layout.setContentsMargins(5, 0, 0, 0)
-            self.block_inputs_layout.setContentsMargins(5, 0, 0, 0)
-            self.block_inputs_layout.addStretch()
-            self.data_city_layout.addStretch()
+        self.block_inputs_layout.addWidget(self.LABEL_CITY, alignment=core.Qt.AlignmentFlag.AlignLeft)
+        self.block_inputs_layout.addWidget(self.box_city, alignment=core.Qt.AlignmentFlag.AlignLeft)
+        self.data_city_layout.setContentsMargins(5, 0, 0, 0)
+        self.block_inputs_layout.setContentsMargins(5, 0, 0, 0)
+        self.block_inputs_layout.addStretch()
+        self.data_city_layout.addStretch()
 
-            self.data_city_layout.addWidget(self.LABEL)
-            self.data_city_layout.addWidget(self.block_inputs)
+        self.data_city_layout.addWidget(self.LABEL)
+        self.data_city_layout.addWidget(self.block_inputs)
 
-            self.block_inputs_layout.addWidget(self.LABEL_COUNTRY)
-            self.block_inputs_layout.addWidget(self.box_countre)
+        self.block_inputs_layout.addWidget(self.LABEL_COUNTRY)
+        self.block_inputs_layout.addWidget(self.box_countre)
 
-            self.block_inputs_layout.addWidget(self.LABEL_CITY)
-            self.block_inputs_layout.addWidget(self.box_city)
+        self.block_inputs_layout.addWidget(self.LABEL_CITY)
+        self.block_inputs_layout.addWidget(self.box_city)
 
-            self.block_inputs_layout.addWidget(self.LABEL_COORDINATE)
-            self.block_inputs_layout.addWidget(self.box_coordinate)
+        self.block_inputs_layout.addWidget(self.LABEL_COORDINATE)
+        self.block_inputs_layout.addWidget(self.box_coordinate)
 
-            self.search_city_layout.addWidget(self.data_city)
-            self.search_city_layout.addWidget(self.map)
-            
-            self.data_city_layout.addWidget(self.SAVE_BUTTON)
+        self.search_city_layout.addWidget(self.data_city)
+        self.search_city_layout.addWidget(self.map)
+        
+        self.data_city_layout.addWidget(self.SAVE_BUTTON)
 
-            self.search_layout.addWidget(self.search_city)
-            self.search_layout.addWidget(self.added_cities)
-            
+        self.search_layout.addWidget(self.search_city)
+        self.search_layout.addWidget(self.added_cities)
     def size_app_pressed(self):
-        # try:
-            self.SEARCH_CITY.setParent(None)
-            self.LANGUAGE.setParent(None)
-            self.FRAME_IMAGE.setParent(None)
-        # except:
-            self.SIZE_APP = Frame_create(layout = widgets.QVBoxLayout(), width = 544, height = 578, color = 'yellow')
-            self.FRAME_MAIN_LAUYT.addWidget(self.SIZE_APP)
-        # self.SIZE_APP = Frame_create(layout = widgets.QVBoxLayout(), width = 544, height = 578, color = 'yellow')
-        # self.FRAME_MAIN_LAUYT.addWidget(self.SIZE_APP)
+        self.del_prev_card(self.SIZE_APP)
+        self.SIZE_APP = Frame_create(layout = widgets.QVBoxLayout(), width = 544, height = 578, color = 'yellow')
+        self.FRAME_MAIN_LAUYT.addWidget(self.SIZE_APP)
     def app_language_pressed(self):
-        # try:
-            self.SEARCH_CITY.setParent(None)
-            self.SIZE_APP.setParent(None)
-            self.FRAME_IMAGE.setParent(None)
-        # except:
+            self.del_prev_card(self.LANGUAGE)
             self.LANGUAGE = Frame_create(layout = widgets.QVBoxLayout(), width = 544, height = 578, color = 'red')
             self.FRAME_MAIN_LAUYT.addWidget(self.LANGUAGE)
-        # self.LANGUAGE = Frame_create(layout = widgets.QVBoxLayout(), width = 544, height = 578, color = 'red')
-        # self.FRAME_MAIN_LAUYT.addWidget(self.LANGUAGE)
     def image_list_pressed(self):
-        # try:
-            self.SIZE_APP.setParent(None)
-            self.LANGUAGE.setParent(None)
-            self.SEARCH_CITY.setParent(None)
-        # except:
-            self.FRAME_IMAGE = Frame_create(layout = widgets.QVBoxLayout(), width = 544, height = 578, color = 'blue')
-            self.FRAME_MAIN_LAUYT.addWidget(self.FRAME_IMAGE)
-        # self.FRAME_IMAGE = Frame_create(layout = widgets.QVBoxLayout(), width = 544, height = 578, color = 'blue')
-        # self.FRAME_MAIN_LAUYT.addWidget(self.FRAME_IMAGE)
+        self.del_prev_card(self.FRAME_IMAGE)
+        self.FRAME_IMAGE = Frame_create(layout = widgets.QVBoxLayout(), width = 544, height = 578, color = 'blue')
+        self.FRAME_MAIN_LAUYT.addWidget(self.FRAME_IMAGE)
 
+
+    def del_prev_card(self, name_card):
+        list_cards = [(self.SEARCH_CITY, self.search_city), (self.LANGUAGE, self.APP_LANGUAGE), (self.FRAME_IMAGE, self.IMAGE_LIST), (self.SIZE_APP, self.size_app)]
+        for card, btn in list_cards:
+            # if card != "":
+            if card != "" and name_card != card:
+                card.setParent(None)
+                btn.setStyleSheet('text-align: left; background-color: transparent')
+            if name_card == card:
+                btn.setStyleSheet("background-color: rgba(0, 0, 0, 100); border-radius: 5px")
